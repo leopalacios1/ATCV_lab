ecfd2641eecbf58810e82bb0e325f9f6a50a9d16
cexu <cexuzhengnnopop@gmail.com>
2022-04-15 11:28:39 +0200

    main script init and kmeans with generic distance

---
+++ Lab2/script.m
@@ -0,0 +1,9 @@
+clear; clc; close all;
+
+%% 
+
+Im = imread( "color pictures/flowers.tif");
+% Im = imread( "color pictures/papillary_carciroma.png");
+% Im = imread( "color pictures/peppers_color.tif");
+% Im = imread( "color pictures/tucan1.jpg");
+
