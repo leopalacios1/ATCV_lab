5e8234aadc6d463eb49f9e177a8e0fc062d813f2
cexu <cexuzhengnnopop@gmail.com>
2022-04-15 18:34:13 +0200

    HSV works bad, mean update with good results

--- Lab2/k_means.m
+++ Lab2/k_means.m
@@ -1,4 +1,4 @@
-function [indx, centers] = k_means(X, k, dist_funct)
+function [indx, centers] = k_means(X, k, dist_funct, mean_funct)
 % we assume X is [n, dims] size
 % set tolerance and maximum number of iterations;
 tol = 1e-4; max_it = 20;
@@ -26,7 +26,7 @@ while(curr_it < max_it && max(dist_funct(centers, prev_centers)) > tol )
     [m,indx] = min(dist_matrix, [], 2); % the index goes from [1,k]
 
     for i = 1:k         % update the centers
-        centers(i,:) = mean( X( indx==i,:), 1 );   % choose the Euclidean mean function
+        centers(i,:) = mean_funct( X( indx==i,:) );
     end
 
     curr_it = curr_it + 1;
--- Lab2/script.m
+++ Lab2/script.m
@@ -1,7 +1,7 @@
 clear; clc; close all; 
 
-Im = imread( "color pictures/flowers.tif");
-% Im = imread( "color pictures/papillary_carciroma.png");
+% Im = imread( "color pictures/flowers.tif");
+Im = imread( "color pictures/papillary_carciroma.png");
 % Im = imread( "color pictures/peppers_color.tif");
 % Im = imread( "color pictures/tucan1.jpg");
 
@@ -10,7 +10,7 @@ X = double(reshape(Im, [size(Im,1)*size(Im,2), size(Im,3)]));
 
 k = 5;
 
-[indx, centers] = k_means(X, k, @eucl_distance);
+[indx, centers] = k_means(X, k, @eucl_distance, @RGB_mean);
 
 Y = zeros(size(X));
 for i = 1:k
@@ -34,7 +34,7 @@ XX = rgb2HSL(X);
 
 k = 5;
 
-[indx, centers] = k_means(XX, k, @eucl_distance);
+[indx, centers] = k_means(XX, k, @HSL_dist, @HSL_mean);
 centers = HSL2rgb(centers);
 
 Y = zeros(size(X));
@@ -53,7 +53,7 @@ imshow(Y)
 title(strcat(" Clusterized k = ", num2str(k)))
 
 
-%%
+%% HSL example
 
 X = double(reshape(Im, [size(Im,1)*size(Im,2), size(Im,3)]));
 XX = rgb2HSL(X);
@@ -67,5 +67,32 @@ title("Original image")
 subplot(1,2,2)
 imshow(Y)
 title(strcat(" Clusterized k = ", num2str(k)))
+
+
+%% HSV distance
+
+XX = rgb2hsv(Im);
+X = double(reshape(XX, [size(Im,1)*size(Im,2), size(Im,3)]));
+
+k = 5;
+
+[indx, centers] = k_means(X, k, @HSV_dist, @HSV_mean);
+centers = hsv2rgb([centers])*255;
+
+Y = zeros(size(X));
+for i = 1:k
+    Y(indx == i,:) = repmat( centers(i,:) , [sum(indx == i),1] );
+end
+
+Y = uint8(  reshape(Y,  [size(Im,1),size(Im,2),size(Im,3)])  );
+
+figure;
+subplot(1,2,1)
+imshow(Im) 
+title("Original image")
+subplot(1,2,2)
+imshow(Y)
+title(strcat(" Clusterized k = ", num2str(k)))
+
 
 
